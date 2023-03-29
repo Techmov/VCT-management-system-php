a9cad86a2025259268080f38459e7617b886c2e5
Techmov <108125390+Techmov@users.noreply.github.com>
2023-03-29 17:45:43 +0300

    Add files via upload

---
+++ database/db_healthcare.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2023 at 04:27 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_healthcare`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appId` int(3) NOT NULL,
+  `patientIc` bigint(12) NOT NULL,
+  `scheduleId` int(10) NOT NULL,
+  `appSymptom` varchar(100) NOT NULL,
+  `appComment` varchar(100) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'process'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`appId`, `patientIc`, `scheduleId`, `appSymptom`, `appComment`, `status`) VALUES
+(87, 710633658, 47, 'blood losss', 'urgent', 'done'),
+(91, 710633658, 49, 'blood losss', 'hiv test', 'process');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `icDoctor` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `doctorId` int(3) NOT NULL,
+  `doctorFirstName` varchar(50) NOT NULL,
+  `doctorLastName` varchar(50) NOT NULL,
+  `doctorAddress` varchar(100) NOT NULL,
+  `doctorPhone` varchar(15) NOT NULL,
+  `doctorEmail` varchar(20) NOT NULL,
+  `doctorDOB` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`icDoctor`, `password`, `doctorId`, `doctorFirstName`, `doctorLastName`, `doctorAddress`, `doctorPhone`, `doctorEmail`, `doctorDOB`) VALUES
+(123456789, '123', 123, 'Doctor', 'cliff', 'nairobi kenya', '0173567758', 'cliff@gmail.com', '1990-04-10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorschedule`
+--
+
+CREATE TABLE `doctorschedule` (
+  `scheduleId` int(11) NOT NULL,
+  `scheduleDate` date NOT NULL,
+  `scheduleDay` varchar(15) NOT NULL,
+  `startTime` time NOT NULL,
+  `endTime` time NOT NULL,
+  `bookAvail` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `doctorschedule`
+--
+
+INSERT INTO `doctorschedule` (`scheduleId`, `scheduleDate`, `scheduleDay`, `startTime`, `endTime`, `bookAvail`) VALUES
+(40, '2015-12-13', 'Sunday', '09:00:00', '10:00:00', 'notavail'),
+(41, '2015-12-13', 'Sunday', '10:00:00', '11:00:00', 'available'),
+(47, '2023-03-30', 'Thursday', '08:00:00', '10:30:00', 'notavail'),
+(49, '2023-03-30', 'Thursday', '11:05:00', '14:05:00', 'notavail'),
+(50, '0000-00-00', 'Thursday', '11:05:00', '14:05:00', 'available'),
+(52, '0000-00-00', '', '00:00:00', '00:00:00', ''),
+(53, '0000-00-00', 'Thursday', '11:05:00', '14:05:00', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `icPatient` bigint(12) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `patientFirstName` varchar(20) NOT NULL,
+  `patientLastName` varchar(20) NOT NULL,
+  `patientMaritialStatus` varchar(10) NOT NULL,
+  `patientDOB` date NOT NULL,
+  `patientGender` varchar(10) NOT NULL,
+  `patientAddress` varchar(100) NOT NULL,
+  `patientPhone` varchar(15) NOT NULL,
+  `patientEmail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`icPatient`, `password`, `patientFirstName`, `patientLastName`, `patientMaritialStatus`, `patientDOB`, `patientGender`, `patientAddress`, `patientPhone`, `patientEmail`) VALUES
+(710633658, '123', 'caleb', 'kiptoo', 'single', '1999-01-01', 'male', 'eldoret kenya', '710633659', 'waititu@gmail.com'),
+(727969971, '123', 'jerry', 'tom', '', '1999-01-01', 'male', '', '', 'calebkip200@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appId`),
+  ADD UNIQUE KEY `scheduleId_2` (`scheduleId`),
+  ADD KEY `patientIc` (`patientIc`),
+  ADD KEY `scheduleId` (`scheduleId`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`icDoctor`);
+
+--
+-- Indexes for table `doctorschedule`
+--
+ALTER TABLE `doctorschedule`
+  ADD PRIMARY KEY (`scheduleId`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`icPatient`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appId` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+
+--
+-- AUTO_INCREMENT for table `doctorschedule`
+--
+ALTER TABLE `doctorschedule`
+  MODIFY `scheduleId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_4` FOREIGN KEY (`patientIc`) REFERENCES `patient` (`icPatient`),
+  ADD CONSTRAINT `appointment_ibfk_5` FOREIGN KEY (`scheduleId`) REFERENCES `doctorschedule` (`scheduleId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
